bfaf8e783b485b7c972f1013b7b864849cf0c2da
Felipe Brito <felipedebritolira@gmail.com>
2018-09-14 08:16:04 -0300

    Incluido parse de tipos complexos e listas.

--- src/main/java/upbi/core/conversor/GenericConvertEntity.java
+++ src/main/java/upbi/core/conversor/GenericConvertEntity.java
@@ -1,7 +1,11 @@
 package upbi.core.conversor;
 
+import br.com.twsoftware.alfred.object.Objeto;
 import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import org.apache.commons.text.WordUtils;
 import upbi.core.execoes.GenericConvertEntityException;
 
@@ -10,13 +14,19 @@ import upbi.core.execoes.GenericConvertEntityException;
  *
  * @author Felipe Brito
  * @param <T> Tipo de entidade de origem
- * @param <E> Tipode de entidade de destino
+ * @param <E> Tipo de de entidade de destino
  */
 public class GenericConvertEntity<T, E> {
 
     private final String GET = "get";
     private final String SET = "set";
     private final String SERIAL_VERSION_IGNORE = "serialVersionUID";
+
+    /**
+     * Classes de tipo simples
+     */
+    private final String[] SIMPLE = {"java.math.BigDecimal", "java.lang.Long", "java.lang.String", "java.lang.Integer", "java.time.LocalDate", "java.lang.Boolean", "java.time.LocalDateTime", "java.lang.Float", "java.util.Date"};
+    private final String[] COMPLEX = {"java.util.List"};
 
     /**
      *
@@ -27,19 +37,47 @@ public class GenericConvertEntity<T, E> {
      */
     public E convertModel(T source, Class<E> destination) throws GenericConvertEntityException {
 
-        //acressentar validação para tipo complexo
-        //adicionar converrsão para List
         try {
 
             Object destino = newInstance(destination);
 
-            if (source == null) {
+            if (Objeto.isBlank(source)) {
                 return (E) destino;
             }
 
             for (Field field : source.getClass().getDeclaredFields()) {
+
                 if (!SERIAL_VERSION_IGNORE.equals(field.getName())) {
-                    Object value = getValue(source, field);
+
+                    Object value = null;
+
+                    if (isSimpleType(field)) {
+
+                        value = getValue(source, field);
+
+                    } else {
+
+                        if (isListType(field)) {
+
+                            Object elements = getValue(source, field);
+                            List array = new ArrayList();
+
+                            if (elements != null) {
+                                iterable(elements, array);
+                                value = array;
+                            }
+
+                        } else {
+
+                            Object obj = getValue(source, field);
+
+                            if (obj != null) {
+                                value = convertModel((T) obj, (Class<E>) obj.getClass());
+                            }
+
+                        }
+                    }
+
                     if (value != null) {
                         setValue(value, destino, field);
                     }
@@ -54,6 +92,15 @@ public class GenericConvertEntity<T, E> {
 
     }
 
+    private void iterable(Object elements, List array) throws GenericConvertEntityException {
+        for (Object obj : (List) elements) {
+            Object element = convertModel((T) obj, (Class<E>) obj.getClass());
+            if (Objeto.notBlank(element)) {
+                array.add(element);
+            }
+        }
+    }
+
     private Object newInstance(Class<E> destination) throws ClassNotFoundException, InstantiationException, IllegalAccessException {
         return Class.forName(destination.getCanonicalName()).newInstance();
     }
@@ -64,5 +111,13 @@ public class GenericConvertEntity<T, E> {
 
     private void setValue(Object value, Object destino, Field field) throws NoSuchMethodException, IllegalAccessException, IllegalArgumentException, InvocationTargetException {
         destino.getClass().getMethod(SET.concat(WordUtils.capitalize(field.getName())), field.getType()).invoke(destino, value);
+    }
+
+    private Boolean isSimpleType(Field field) {
+        return Arrays.asList(SIMPLE).contains(field.getType().toString().replace("class", "").trim());
+    }
+
+    private Boolean isListType(Field field) {
+        return Arrays.asList(COMPLEX).contains(field.getType().toString().replace("interface", "").trim());
     }
 }
